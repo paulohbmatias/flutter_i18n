4a2a971e809078edb63fa935a1ec12c8d09e8145
Paulo Matias <paulohbmatiasdev@gmail.com>
2021-08-03 15:38:30 -0300

    first commit

---
+++ lib/l10n/app_localizations_pt.dart
@@ -0,0 +1,14 @@
+
+
+
+import 'app_localizations.dart';
+
+/// The translations for Portuguese (`pt`).
+class AppLocalizationsPt extends AppLocalizations {
+  AppLocalizationsPt([String locale = 'pt']) : super(locale);
+
+  @override
+  String hello(String nome) {
+    return 'Olá $nome';
+  }
+}
